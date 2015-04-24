bad26bb4d6313f9a94bdedc8b0297937bb5a5d8c
llogiq <bogusandre@gmail.com>
2015-04-24 11:50:18 +0200

    Ignore Cargo.lock, created comparison for literals (because == doesn't work)

--- src/eq_op.rs
+++ src/eq_op.rs
@@ -15,6 +15,7 @@ extern crate rustc;
 use rustc::plugin::Registry;
 use rustc::lint::*;
 use syntax::ast as ast;
+use syntax::ast_util as ast_util;
 
 declare_lint! {
     EQ_OP,
@@ -33,28 +34,18 @@ impl LintPass for EqOp {
     fn check_expr(&mut self, cx: &Context, e: &ast::Expr) {
         // ExprBinary(BinOp, P<Expr>, P<Expr>)
         if let ast::ExprBinary(ref op, ref left, ref right) = e.node {
-            if left == right && is_cmp_or_bit(op) {
-                cx.span_lint(EQ_OP, e.span, &format!("equal expressions as operands to {}", fmt_op(op)));
+            if is_cmp_or_bit(op) && is_node_equal(left, right) {
+                cx.span_lint(EQ_OP, e.span, &format!("equal expressions as operands to {}", ast_util::binop_to_string(op.node)));
             }
         }
     }
 }
 
-fn fmt_op(op : &ast::BinOp) -> &str {
-	match op.node {
-		ast::BiEq => "==",
-        ast::BiLt => "<",
-        ast::BiLe => "<=",
-        ast::BiGt => ">",
-        ast::BiGe => ">=",
-        ast::BiNe => "!=",
-        ast::BiAnd => "&&",
-		ast::BiOr => "||",
-		ast::BiBitXor => "^",
-		ast::BiBitAnd => "&",
-		ast::BiBitOr => "|",
-        _ => "?"
-    }
+fn is_node_equal(left : &ast::Expr, right : &ast::Expr) -> bool {
+	match (&left.node, &right.node) {
+		(&ast::ExprLit(ref lit_left), &ast::ExprLit(ref lit_right)) => lit_left.node == lit_right.node,
+		_ => false
+	}
 }
 
 fn is_cmp_or_bit(op : &ast::BinOp) -> bool {
